8ba3fdb39488f65542c144b63af1d932b0320f85
Zoltan Benyo <momanten@gmail.com>
2024-09-27 10:45:44 +0200

    remove mapping from utilityServices

--- src/app/services/utility.service.ts
+++ src/app/services/utility.service.ts
@@ -21,21 +21,4 @@ export class UtilityService {
     return date.split('/').join('.');
   }
 
-  createCoursesWithAuthorNames(courses:MockCourseData[], authors:Author[]):CourseData[] {
-    if (courses.length > 0) { 
-      return courses.map(course => ({
-        id: course.id,
-        title: course.title,
-        description: course.description,
-        date: course.creationDate, // 'creationDate' to 'date'
-        duration: course.duration,
-        authors: course.authors.map(authorId => {
-          const author = authors.find(a => a.id === authorId);
-          return author ? { id: author.id, name: author.name } : { id: authorId, name: 'Unknown' };
-        })
-      })) 
-    }
-    else return [];
-  }
-
 }
