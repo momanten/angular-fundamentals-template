1f0ba3e4a2521628aa848e879fbea34af427c54f
Zoltan Benyo <momanten@gmail.com>
2024-10-01 08:02:31 +0200

    Simple entity (courses used through) the courses

--- src/app/features/courses/courses.component.ts
+++ src/app/features/courses/courses.component.ts
@@ -1,7 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { MappingService } from '@app/services/mapping.service';
-import { Author } from '@app/shared/types/author.model';
-import { Course, CourseInfo } from '@app/shared/types/course.model';
+import { CourseInfo } from '@app/shared/types/course.model';
 
 @Component({
   selector: 'app-courses',
@@ -9,21 +7,15 @@ import { Course, CourseInfo } from '@app/shared/types/course.model';
   styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent {
-  @Input() courses:Course[]=[];
-  @Input() authors:Author[]=[];
+  @Input() courses:CourseInfo[]=[];
   
   courseInfo:CourseInfo|undefined=undefined;
 
   readonly emptyListTitle = 'Your List is Empty';
   readonly emptyListText = "Please use 'ADD NEW COURSE' button to add your first course";
 
-  constructor (private mappingService:MappingService) {}
-
   handleShowCourseInfo(courseId:string) {
-    const foundCourse = this.courses.find((course)=>course.id===courseId);
-    if (foundCourse) {
-      this.courseInfo = this.mappingService.createCourseWithAuthorNames(foundCourse, this.authors);
-    }
+    this.courseInfo = this.courses.find((course)=>course.id===courseId);
   }
 
   handleShowCourses():void {
